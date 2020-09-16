44fd082195d6ee29a138741544a3d32071abf276
FabrizioVx <fabrizio.vasquez@utec.edu.pe>
2020-09-16 11:55:15 -0500

    app

--- src/main/java/cs/lab/DNASequencer.java
+++ src/main/java/cs/lab/DNASequencer.java
@@ -1,7 +1,10 @@
 package cs.lab;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.function.Supplier;
 import java.util.logging.Logger;
+import java.util.HashMap;
 
 public class  DNASequencer {
 
@@ -11,6 +14,22 @@ public class  DNASequencer {
     }
 
     public String calculate(List<String> part){
+        List<String> sequences;
+        HashMap<String,Integer> HashSequence = new HashMap<String,Integer>();
+        StringBuilder DNASequence = new StringBuilder();
+        for(int i = 0; i < part.size();++i){
+            String sequence = part.get(i);
+            for( int j = 0 ;j < sequence.length();++j){
+                String valueSequence = String.valueOf(sequence.charAt(j));
+                HashSequence.put(j,valueSequence);
+                DNASequence.append(String.valueOf(sequence.charAt(j)));
+                logger.warning(String.valueOf(DNASequence));
+            }
+        }
+        for (DNASequence:)
+
+
+
         return "AGATTACAGA";
     }
 }
