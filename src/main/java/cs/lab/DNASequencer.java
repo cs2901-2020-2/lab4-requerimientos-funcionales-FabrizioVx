6dede27fdad4100c4611a7afd84d5fb07dca1765
FabrizioVx <fabrizio.vasquez@utec.edu.pe>
2020-09-17 08:43:25 -0500

    sonarCloud

--- src/main/java/cs/lab/DNASequencer.java
+++ src/main/java/cs/lab/DNASequencer.java
@@ -4,32 +4,26 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.function.Supplier;
 import java.util.logging.Logger;
-import java.util.HashMap;
 
 public class  DNASequencer {
-
+    static final Integer FIRST_SEQUENCE = 0;
     static final Logger logger = Logger.getLogger(DNASequencer.class.getName());
     public DNASequencer() {
         logger.info("Starting sequencer...");
     }
 
     public String calculate(List<String> part){
-        List<String> sequences;
-        HashMap<String,Integer> HashSequence = new HashMap<String,Integer>();
-        StringBuilder DNASequence = new StringBuilder();
-        for(int i = 0; i < part.size();++i){
-            String sequence = part.get(i);
-            for( int j = 0 ;j < sequence.length();++j){
-                String valueSequence = String.valueOf(sequence.charAt(j));
-                HashSequence.put(j,valueSequence);
-                DNASequence.append(String.valueOf(sequence.charAt(j)));
-                logger.warning(String.valueOf(DNASequence));
+        String firstSubSequence = part.get(FIRST_SEQUENCE);
+        String SequenceConcat = "" ;
+        for(int iteratorOfSequence = 1; iteratorOfSequence < part.size();++iteratorOfSequence){
+            String subSequenceActive;
+            subSequenceActive = part.get(iteratorOfSequence);
+            for(int iteratorOfSubSequence = 0 ;iteratorOfSubSequence < subSequenceActive.length();++iteratorOfSubSequence){
+                if(!firstSubSequence.contains(subSequenceActive.substring(iteratorOfSubSequence))){
+                    SequenceConcat = firstSubSequence.concat(subSequenceActive.substring(iteratorOfSubSequence));
+                }
             }
         }
-        for (DNASequence:)
-
-
-
-        return "AGATTACAGA";
+        return SequenceConcat;
     }
 }
